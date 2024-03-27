9d7467e443b1a3a148d9234085d06b3bd8783d29
Никита Васильняк <nvasil94@bk.ru>
2024-03-28 01:17:20 +0300

    Добавил смену цветовой схемы, изменил css, js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import { BodyMassIndex } from './components/bmi';
 import './css/styles.css';
 import './index.html';
 
-let bodyMassIndx = 0;
-let idealWeight = 0;
-
 //Принимаем данные из формы
 const calculation = document.querySelector('.calculate-button');
 const modalElement = document.getElementById('modal');
@@ -16,7 +13,6 @@ const met = document.querySelector('.metabolism');
 
 
 calculation.addEventListener('click', getFormValue);
-
 function getFormValue(event) {
   event.preventDefault();
   const data = {};
@@ -31,33 +27,37 @@ function getFormValue(event) {
   let bodyMassIndex = BodyMassIndex.create(Number(data.height), Number(data.weight),
                                             Number(data.age), data.gender, Number(data.activity));
   
-  // console.log(bodyMassIndex.calculateBMI());
-  // console.log(bodyMassIndex.idealWeight());
-  // console.log(bodyMassIndex.basicMetabolicRate());
-  // console.log(bodyMassIndex.metabolism());
   modalElement.classList.remove('visually-hidden');
-  // modalElement.setAttribute('style', 'display: flex;')
+
   bmi.innerHTML = ` ${bodyMassIndex.calculateBMI()}`;
   iweight.innerHTML = ` ${bodyMassIndex.idealWeight()} `;
   blm.innerHTML = ` ${bodyMassIndex.basicLevelMetabolic()} `
   bmr.innerHTML = ` ${bodyMassIndex.basicMetabolicRate()} `;
   met.innerHTML = ` ${bodyMassIndex.metabolism()} `;
+
   modalElement.showModal();
 }
 
-
 modalElement.addEventListener("click", closeOnBackDropClick)
-
 function closeOnBackDropClick({ currentTarget, target }) {
   const dialogElement = currentTarget
   const isClickedOnBackDrop = target === dialogElement
   if (isClickedOnBackDrop) {
     modalElement.close()
-    // modalElement.setAttribute('style', 'display: none;')
     modalElement.classList.add('visually-hidden');
-    // bmi.innerHTML = `Индекс массы тела: `;
-    // iweight.innerHTML = `Идеальный вес: `;
-    bmr.innerHTML = `Базовая скорость метаболизма: `;
-    met.innerHTML = `Метаболизм:`;
+    bmi.innerHTML = ' ';
+    iweight.innerHTML = ' ';
+    blm.innerHTML = ' ';
+    bmr.innerHTML = ' ';
+    met.innerHTML = ' ';
   }
 }
+
+let page = document.querySelector('.page');
+let changeButton = document.querySelector('.change-button');
+
+changeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
+
